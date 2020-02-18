9010702e2775c3f8c0e3ce722f526bad7ffbba56
Misael <misaelbrunoroliberato@gmail.com>
2020-02-17 23:45:14 -0300

    updates

--- src/config/server.js
+++ src/config/server.js
@@ -20,6 +20,12 @@ var io = require('socket.io').listen(app);
 
 io.sockets.on('connection', function (socket) {
     console.log('new connection made.')
+
+    socket.on('join', function(data) {
+        socket.join(data.room);
+        console.log(data.user + 'joined the room: '+ data.room);
+        socket.broadcast.to(data.room).emit('new user joined', {user:data.user, message:'has joined this room.'});
+    }); 
 });
 
 module.exports = server;
